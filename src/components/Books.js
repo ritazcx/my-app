bb551e0c0e97e24df4b66be93a70b88990919f26
ritaz <ritazcx2017@outlook.com>
2021-10-22 00:00:36 +0800

    1.0.2

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,15 @@
 const Books = ({url}) => {
     const myRequest = new Request(url);
-    let content;
+    let books;
     fetch(myRequest)
-    .then((res) => res.json())
-    .then((data) => { 
-        // console.log(data.results.books);
+        .then((res) => res.json())
+        .then((data) => {
+            books = data.results.books.map((book) => 
+        <li key={book.title}>
+            {book.title}
+        </li>)});
 
-        content = data.results.books.map((book) => 
-            <li key={book.title}>
-                {book.title}
-            </li>
-        );
-
-        console.log(content);
+        console.log(books);
         // content = data.results.books.map((book) => 
         // <div key={book.title.split(' ').join('_')}>
         //     <h3 className="booktitle">{book.title}</h3>
@@ -20,12 +17,11 @@ const Books = ({url}) => {
         //     <p>{book.description}</p>
         //     <p>ISBN: {book.primary_isbn10}</p>
         // </div>)
-    })
+    // })
     // const books = props.results.Books;
 
     return (
-        // <div>{url}</div>
-        <ul>{content}</ul>
+        <ul>{books}</ul>
     )
 }
 
